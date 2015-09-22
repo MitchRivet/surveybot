9e69019c81d439a90b5fac9ca7ea5f78fc498af6
Mitch Rivet & Deron Johnson <student@epicodus.com>
2015-09-22 16:19:56 -0700

    Display results as number of times particular choices were selected

--- services/SurveyFactory.js
+++ services/SurveyFactory.js
@@ -1,27 +1,32 @@
 surveybot.factory('SurveyFactory', function SurveyFactory() {
   var factory = {};
-
   factory.answer1 = [];
-
   factory.answer2 = [];
 
   factory.addAnswerOne = function() {
     var answerOne = { result: factory.wineAnswer };
     factory.answer1.push(answerOne);
+    factory.wineAnswer = null;
     };
 
   factory.addAnswerTwo = function() {
     var answerTwo = { result: factory.clooneyAnswer };
     factory.answer2.push(answerTwo);
+    factory.clooneyAnswer = null;
   };
 
+  // countwhite = 0;
+  // countred = 0;
+  // countcry = 0;
   // factory.displayWine = function () {
-  //   return factory.answer1;
-  // };
+  //   answer1.forEach(function(wine) {
+  //     if (wine === "white") {
+  //       countwhite++;
+  //     }
+  //   })
   //
-  // factory.displayClooney = function () {
-  //   return factory.answer2;
+  //   return countwhite;
   // };
 
-    return factory;
+  return factory;
 });
